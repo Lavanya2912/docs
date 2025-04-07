958857895283aad141548cefb42f3ee5a96c59a2
lavanyavasavi2912@gmail.com <lavanyavasavi2912@gmail.com>
2025-04-07 12:16:14 +0530

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NzcxMg/spectral.js?branch_slug=main&token=93200e28-9cb8-4651-a6b0-caa62e70d9d9";
+export default { extends: ruleset };
